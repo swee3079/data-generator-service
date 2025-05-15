43658b87f02b10fdb14ec93d2c4de1ab4e49db6c
Sisuka Weerasinghe <sisuka.weerasinghe@sysco.com>
2025-05-15 10:16:12 +0530

    Initial commit

---
+++ src/main/java/com/sysco/test_data_generator/TestDataGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.sysco.test_data_generator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestDataGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestDataGeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sysco/test_data_generator/TestDataGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sysco.test_data_generator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestDataGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
